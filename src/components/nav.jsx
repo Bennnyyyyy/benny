50ac534e48aa3a6a61ee3cdc7ab8b1a1b9294d2c
Bennnyyyyy <benladindinin0909@gmail.com>
2025-03-06 21:39:36 +0800

    i fix the nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,26 @@
 import { Link } from 'react-router-dom';
-import '../styles/nav.css'; 
+import { useState } from 'react';
+import '../styles/nav.css';
+import logo from "../assets/navlogo.png";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className={`hamburger-menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><a href="#contact">Contact</a></li>
